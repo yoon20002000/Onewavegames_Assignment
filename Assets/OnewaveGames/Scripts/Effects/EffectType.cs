d2f47a9519fc2604e2253faa4ac0ab85995e5bce
yoon20002000 <yoon20002000@naver.com>
2025-08-09 11:35:28 +0900

    미구현 Type 제거

--- Assets/OnewaveGames/Scripts/Effects/EffectType.cs
+++ Assets/OnewaveGames/Scripts/Effects/EffectType.cs
@@ -3,7 +3,5 @@ public enum EEffectType
     Projectile,     // 투사체 발사
     Pull,           // 끌어오기
     Damage,         // 데미지
-    Stun,           // 기절
-    Slow,           // 둔화
     Heal            // 회복
 }
